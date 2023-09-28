a04174989bce1b5559198d9816cac9997c2670de
yazj <yuanyi2000@protonmail.com>
2023-09-28 11:31:08 +0800

    dap keymap

--- keymaps.md
+++ keymaps.md
@@ -6,3 +6,17 @@
 |在neotree H| 显示隐藏文件 |
 | \<leader\>bp bP | 固定buffer 清除所有未固定buffer |
 | \<leader\>cR | 头文件源文件替换 |
+
+---
+
+## Dap
+
+|Key | function |
+|:---:|:---:|
+|F6 |打断点 |
+|F5|调试 |
+|F4|停止调试 |
+|F3|清除断点 |
+|F12|step over |
+|F7|step info |
+|\<leader\>de |eval() |
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -36,3 +36,27 @@ keymap.set("n", "<leader>hw", ":HopWord<cr>")
 keymap.set("n", "<leader>hww", ":HopWordMW<cr>")
 keymap.set("n", "<leader>hc", ":HopChar1<cr>")
 keymap.set("n", "<leader>hcw", ":HopChar1MW<cr>")
+
+--  insert模式下，跳到行首行尾
+keymap.set("i", "<C-h>", "<ESC>I")
+keymap.set("i", "<C-l>", "<ESC>A")
+
+-- Dap
+keymap.set(
+  "n",
+  "<F4>",
+  ":lua require'dap'.close()<CR>"
+    .. ":lua require'dap'.terminate()<CR>"
+    .. ":lua require'dap.repl'.close()<CR>"
+    .. ":lua require'dapui'.close()<CR>"
+    .. ":lua require('dap').clear_breakpoints()<CR>"
+    .. "<C-w>o<CR>"
+)
+
+keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
+keymap.set("n", "<F6>", ":lua require'dap'.toggle_breakpoint()<CR>")
+keymap.set("n", "<F3>", ":lua require'dap'.clear_breakpoints()<CR>")
+keymap.set("n", "<F12>", ":lua require'dap'.step_over()<CR>")
+keymap.set("n", "<F2>", ":lua require'dap'.step_out()<CR>")
+keymap.set("n", "<F7>", ":lua require'dap'.step_into()<CR>")
+keymap.set("n", "<C-F12>", ":lua require'dapui'.eval()<CR>")
