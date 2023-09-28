de1017e87905f979d093ece2df849ba7cb1b5787
yazj <yuanyi2000@protonmail.com>
2023-09-28 10:57:08 +0800

    use dap to debug

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -2,4 +2,6 @@ return {
   { "tpope/vim-surround" },
   { "christoomey/vim-tmux-navigator" },
   { "akinsho/toggleterm.nvim", version = "*", config = true },
+  { "HUAHUAI23/telescope-dapzzzz" },
+  { "nvim-lua/plenary.nvim" },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,12 @@ return {
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
+    requires = { { "nvim-lua/plenary.nvim" }, { "HUAHUAI23/telescope-dapzzzz" } },
     config = function()
       local telescope = require("telescope")
       telescope.load_extension("fzf")
+      require("telescope").setup({})
+      require("telescope").load_extension("i23")
     end,
     keys = {
       {
