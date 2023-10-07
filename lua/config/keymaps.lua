af0dbb0b4e6d91540bde349dbe3b4267ac42c6dd
yazj <yuanyi339@gmail.com>
2023-10-07 14:10:11 +0800

    new config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,24 @@ local keymap = vim.keymap
 keymap.set("i", "jj", "<Esc>")
 keymap.set("n", "<leader>wv", "<C-W>v")
 keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>")
+
+-- Dap
+
+keymap.set(
+  "n",
+  "<F4>",
+  ":lua require'dap'.close()<CR>"
+    .. ":lua require'dap'.terminate()<CR>"
+    .. ":lua require'dap.repl'.close()<CR>"
+    .. ":lua require'dapui'.close()<CR>"
+    .. ":lua require('dap').clear_breakpoints()<CR>"
+    .. "<C-w>o<CR>"
+)
+
+keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
+keymap.set("n", "<F6>", ":lua require'dap'.toggle_breakpoint()<CR>")
+keymap.set("n", "<F3>", ":lua require'dap'.clear_breakpoints()<CR>")
+keymap.set("n", "<F12>", ":lua require'dap'.step_over()<CR>")
+keymap.set("n", "<F7>", ":lua require'dap'.step_out()<CR>")
+keymap.set("n", "<F10>", ":lua require'dap'.step_into()<CR>")
+keymap.set("n", "<F2>", ":lua require'dapui'.eval()<CR>")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,9 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "plugins" },
   },
   defaults = {
