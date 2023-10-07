a43a40a1f0411c0cdde5657af1ae20e8124e874b
yazj <yuanyi339@gmail.com>
2023-10-07 19:39:40 +0800

    add plugin tabnine

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -47,70 +47,89 @@ local plugins = {
     end,
   },
 
-  -- github copilot
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
+  -- tabnine
 
   {
-    "nvim-lualine/lualine.nvim",
-    optional = true,
-    event = "VeryLazy",
-    opts = function(_, opts)
-      local Util = require "lazyvim.util"
-      local colors = {
-        [""] = Util.fg "Special",
-        ["Normal"] = Util.fg "Special",
-        ["Warning"] = Util.fg "DiagnosticError",
-        ["InProgress"] = Util.fg "DiagnosticWarn",
+    "codota/tabnine-nvim",
+    lazy = false,
+    build = "./dl_binaries.sh",
+    config = function()
+      require("tabnine").setup {
+        disable_auto_comment = true,
+        accept_keymap = "<C-a>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+        log_file_path = nil, -- absolute path to Tabnine log file
       }
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          local icon = require("lazyvim.config").icons.kinds.Copilot
-          local status = require("copilot.api").status.data
-          return icon .. (status.message or "")
-        end,
-        cond = function()
-          local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
-          return ok and #clients > 0
-        end,
-        color = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local status = require("copilot.api").status.data
-          return colors[status.status] or colors[""]
-        end,
-      })
     end,
   },
 
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = "copilot.lua",
-    opts = {},
-    config = function(_, opts)
-      local copilot_cmp = require "copilot_cmp"
-      copilot_cmp.setup(opts)
-      -- attach cmp source whenever copilot attaches
-      -- fixes lazy-loading issues with the copilot cmp source
-      require("lazyvim.util").on_attach(function(client)
-        if client.name == "copilot" then
-          copilot_cmp._on_insert_enter {}
-        end
-      end)
-    end,
-  },
+  -- github copilot
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = { enabled = false },
+  --     panel = { enabled = false },
+  --     filetypes = {
+  --       markdown = true,
+  --       help = true,
+  --     },
+  --   },
+  -- },
+  --
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   optional = true,
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     local Util = require "lazyvim.util"
+  --     local colors = {
+  --       [""] = Util.fg "Special",
+  --       ["Normal"] = Util.fg "Special",
+  --       ["Warning"] = Util.fg "DiagnosticError",
+  --       ["InProgress"] = Util.fg "DiagnosticWarn",
+  --     }
+  --     table.insert(opts.sections.lualine_x, 2, {
+  --       function()
+  --         local icon = require("lazyvim.config").icons.kinds.Copilot
+  --         local status = require("copilot.api").status.data
+  --         return icon .. (status.message or "")
+  --       end,
+  --       cond = function()
+  --         local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+  --         return ok and #clients > 0
+  --       end,
+  --       color = function()
+  --         if not package.loaded["copilot"] then
+  --           return
+  --         end
+  --         local status = require("copilot.api").status.data
+  --         return colors[status.status] or colors[""]
+  --       end,
+  --     })
+  --   end,
+  -- },
+  --
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   dependencies = "copilot.lua",
+  --   opts = {},
+  --   config = function(_, opts)
+  --     local copilot_cmp = require "copilot_cmp"
+  --     copilot_cmp.setup(opts)
+  --     -- attach cmp source whenever copilot attaches
+  --     -- fixes lazy-loading issues with the copilot cmp source
+  --     require("lazyvim.util").on_attach(function(client)
+  --       if client.name == "copilot" then
+  --         copilot_cmp._on_insert_enter {}
+  --       end
+  --     end)
+  --   end,
+  -- },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
